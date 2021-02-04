6d1844c5dbd10594d422b14a6a0e708488f33077
RAMA-MOSA <ramamosa@yandex.ru>
2021-02-05 00:51:03 +0300

    Проектная работа 12 попытка 1

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
+const notFoundRouter = require('./routes/notFound');
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', cardsRoutes);
 app.use('/', usersRoutes);
+app.all('*', notFoundRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
